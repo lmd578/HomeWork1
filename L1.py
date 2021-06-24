b4d8a6c281d516b376d4848add19707a6b841650
lmd578 <73003097+lmd578@users.noreply.github.com>
2021-06-24 19:36:20 +0600

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,10 @@
+
+user_number = int(input('Введите число '))
+
+if user_name == 8:
+    print('введенное число равно 8')
+
+elif user_number >=8:
+    print('число равно 10')
+esle:
+    print('число не равно 8')
